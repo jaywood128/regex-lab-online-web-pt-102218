1b45bbf88fd96e68c41c41fcaa1b11b64d7a2327
Johnathon Wood <jaywood128@gmail.com>
2018-11-04 23:13:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,7 @@
 def starts_with_a_vowel?(word)
-  if word == word.match(/^[aeiouAEIOU]/)
+  word.match(/^[aeiouAEIOU]/)
     true
 end
-end
-
 def words_starting_with_un_and_ending_with_ing(text)
 
 end
